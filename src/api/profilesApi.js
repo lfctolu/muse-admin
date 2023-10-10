1645237e00887f5d64525b6cff237be8d95cac50
Anton Luhavy <anton.luhavy@yellow.systems>
2023-10-10 14:11:50 +0300

    Add users csv

--- src/api/profilesApi.js
+++ src/api/profilesApi.js
@@ -3,6 +3,7 @@ import apiClient from './axiosClient';
 const API = {
   searchProfiles: (params) => apiClient.get('profiles/search', { params }),
   deleteProfile: (id) => apiClient.delete(`profiles/${id}`),
+  generateCsv: () => apiClient.get('profiles/csv', { responseType: 'blob' }),
 };
 
 export default API;
